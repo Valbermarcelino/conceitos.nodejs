1343a08a6d4d15b9dd1d2a89bf8454b8ff66d7b7
valber <valbermarcelino99@gmail.com>
2020-12-27 22:38:53 -0300

    CRUD completo

--- src/app.js
+++ src/app.js
@@ -49,9 +49,11 @@ app.put("/repositories/:id", (request, response) => {
    }
     
   const repository = {
+      id,
       title,
       url, 
       techs, 
+      likes: repositories[repositoryIndex].likes,
   };
 
   repositories[repositoryIndex] = repository;
